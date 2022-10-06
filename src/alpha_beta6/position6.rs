4d447f6294c91fadf6729955c9b9fbfba468d29e
Arghz <dominique.gilleman@gmail.com>
2022-10-06 23:10:07 +0200

    all position6 tests

--- src/alpha_beta6/position6.rs
+++ src/alpha_beta6/position6.rs
@@ -58,7 +58,7 @@ impl Position6 {
         self.bitboard[(self.moves & 1) as usize] ^= mov;
     }
 
-    fn play_sequence (&mut self, seq: String) -> usize {
+    fn play_sequence (&mut self, seq: &str) -> usize {
         for (index_seq, seq_item) in seq.chars().enumerate() { 
             let col = (seq_item.to_digit(10).unwrap() - 1) as usize;
             if col >= Position6::WIDTH || !self.can_play(col) || self.is_winning_move(col) {
@@ -83,5 +83,92 @@ mod tests {
                 assert!(position.can_play(c))
             }
         }
+    }
+
+    #[test]
+    fn play() {
+        let mut position = Position6::new();
+        position.play(0);
+        assert_eq!(position.bitboard[0], 0b1);
+        position.play(0);
+        assert_eq!(position.bitboard[1], 0b10);
+        position.play(0);
+        assert_eq!(position.bitboard[0], 0b101);
+        position.play(0);
+        assert_eq!(position.bitboard[1], 0b1010);
+
+        position = Position6::new();
+        position.play(1);
+        assert_eq!(position.bitboard[0], 0b10000000);
+        position.play(1);
+        assert_eq!(position.bitboard[1], 0b100000000);
+    }
+
+    #[test]
+    fn unplay() {
+        let mut position = Position6::new();
+        position.play(0);
+        assert_eq!(position.bitboard[0], 0b1);
+        position.unplay(0);
+        assert_eq!(position.bitboard[0], 0b0);
+        position.play(1);
+        assert_eq!(position.bitboard[0], 0b10000000);
+        position.unplay(1);
+        assert_eq!(position.bitboard[0], 0b0);
+
+        for _i in 0..3 {
+            position.play(0);
+        }
+            
+        assert_eq!(position.bitboard[0], 0b101);
+        assert_eq!(position.bitboard[1], 0b10);
+        position.unplay(0);
+        assert_eq!(position.bitboard[0], 0b1);
+        assert_eq!(position.bitboard[1], 0b10);
+        position.unplay(0);
+        assert_eq!(position.bitboard[0], 0b1);
+        assert_eq!(position.bitboard[1], 0b0);
+    }
+
+    #[test]
+    fn is_winning_move_horizontal() {
+        let mut position = Position6::new();
+        let l = position.play_sequence("334323"); //col: 223213
+        /*
+        .......
+        .......
+        .......
+        ..2....
+        ..22...
+        .111...
+        */
+        assert_eq!(l, 6);
+    
+        let mut result = position.is_winning_move(0);
+        assert!(result);
+        result = position.is_winning_move(4);
+        assert!(result);
+        result = position.is_winning_move(5);
+        assert!(!result);
+        result = position.is_winning_move(6);
+        assert!(!result);
+    }
+    
+    #[test]
+    fn is_winning_move_diag1() {
+        let mut position = Position6::new();
+        let l = position.play_sequence("5443354556"); //col: 4332243445
+        /*
+        .......
+        .......
+        ....1..
+        ...12..
+        ..112..
+        ..2212.
+        */
+        assert_eq!(l, 10);
+    
+        let result = position.is_winning_move(1);
+        assert!(result);
     }
 }
